4643481f54160a7d37a6b2d0442577de417bd69f
Divya Raj <divya21raj@gmail.com>
2018-02-18 11:00:28 +0530

    File-Copy-Utility: Argument parsing in place

--- File-Copy-Utility/main.c
+++ File-Copy-Utility/main.c
@@ -1,37 +1,75 @@
 /* 	Name: DIVYA RAJ
-	IDs: 1610110123
+	ID: 1610110123
 */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <memory.h>
 
-int main()
+#define READ_END 0
+#define WRITE_END 1
+
+#define MAX_PATH_LENGTH 128
+
+char *sourcePath;
+char *destinationPath;
+
+void parseArguments(int argc, char* argv[MAX_PATH_LENGTH]);
+
+int main(int argc, char* argv[MAX_PATH_LENGTH])
 {
-	int fd[2];
-	pid_t cpID;
+	int fd[2];   //File descriptor
+	pid_t childID;
+
+	int i;
+	for(i=0; i<argc; i++)
+		printf("%s\n", argv[i]);
+
+	parseArguments(argc, argv);
 
 	pipe(fd); /* create pipe */
-	if((cpID = fork()) == -1)
+	if((childID = fork()) == -1)
 	{
 		perror("fork");
 		exit(1);
 	}
-	if(cpID == 0)
+	if(childID == 0)
 	{
 	/* child only reads. Close the write end */
-		close(fd[1]);
+		close(fd[WRITE_END]);
 	/* Open the destination file and read from pipe */
 	/* Create the destination file if it does not exist */
 	}
 	else
 	{
 	/* parent only writes. Close the read end */
-		close(...);
+		close(READ_END);
 	/* Open the source file and write to pipe */
 	/* Print error message if the source file does not exist */
 	}
 	/* Remember to close the files */
 
 	return 0;
+}
+
+void parseArguments(int argc, char* argv[MAX_PATH_LENGTH])
+{
+	if(argc > 3)
+	{
+		printf("Invalid arguments!\n");
+		exit(0);
+	}
+
+	else if(argc == 1)
+	{
+		printf("No arguments!\n");
+		exit(0);
+	}
+
+	else
+	{
+		sourcePath = strdup(argv[1]);
+		destinationPath = strdup(argv[2]);
+	}
 }
